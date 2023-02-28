3b8e532e9d6ecbccac15c1c82207caee8fe56ec7
Matt Fowler <mbfowler@gmail.com>
2023-02-28 16:23:14 -0500

    Add variables for booleans

--- variables.js
+++ variables.js
@@ -5,3 +5,7 @@ var fabString = "this is a fabulous string, everybody is saying it";
 var cucumberNumber = 1;
 var fidgetDidget = 8;
 var rageAge = 14;
+
+var assignmentComplete = false;
+var sunnyWeather = true;
+var hasCats = true;
