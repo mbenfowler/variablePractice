0d8bba67a4f573dbc74952fef263b3f8224a98c2
Matt Fowler <mbfowler@gmail.com>
2023-02-28 16:43:14 -0500

    Add doesMathEqual function, console logs, and fix spelling

--- variables.js
+++ variables.js
@@ -2,10 +2,24 @@ var greatString = "this is a great string";
 var bestString = "this is the best string";
 var fabString = "this is a fabulous string, everybody is saying it";
 
-var cucumberNumber = 1;
-var fidgetDidget = 8;
+var cucumberNumber = 6;
+var fidgetDigit = 8;
 var rageAge = 14;
+
+function doesMathEqual(num1, num2, num3) {
+    return num1 - num2 === num3
+};
 
 var assignmentComplete = false;
 var sunnyWeather = true;
 var hasCats = true;
+
+console.log(`I really think ${greatString}.`);
+console.log(`But I also hear that ${bestString}.`);
+console.log(`Also, ${fabString}.`);
+
+console.log(`Are both sides equal? ${doesMathEqual(rageAge, fidgetDigit, cucumberNumber)}`);
+
+console.log(`Is this assignment complete? ${assignmentComplete}`)
+console.log(`Is it sunny outside? ${sunnyWeather}`)
+console.log(`Do I have cats? ${hasCats}`)
