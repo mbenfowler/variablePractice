8b16ac8b519ba76251af1566fa65511a9074c0ba
Matt Fowler <mbfowler@gmail.com>
2023-02-28 16:20:59 -0500

    Add variables for numbers

--- variables.js
+++ variables.js
@@ -1,3 +1,7 @@
-stringOne = "this is a great string"
-stringTwo = "this is the best string"
-stringThree = "this is a fabulous string, everybody is saying it"
+var greatString = "this is a great string";
+var bestString = "this is the best string";
+var fabString = "this is a fabulous string, everybody is saying it";
+
+var cucumberNumber = 1;
+var fidgetDidget = 8;
+var rageAge = 14;
